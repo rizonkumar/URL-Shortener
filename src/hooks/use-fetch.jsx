69dda90ffca7048722086f4692467830cbf85b38
rizon.rahi <rizon.rahi@ugamsolutions.com>
2024-06-24 11:40:13 +0530

    Fix: Typo error

--- src/hooks/use-fetch.jsx
+++ src/hooks/use-fetch.jsx
@@ -1,25 +1,25 @@
-const { useState } = require("react");
+import { useState } from "react";
 
 const useFetch = (cb, options = {}) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(null);
-  const [errors, setErrors] = useState(null);
+  const [error, setError] = useState(null);
 
   const fn = async (...args) => {
     setLoading(true);
-    setErrors(null);
+    setError(null);
 
     try {
       const response = await cb(options, ...args);
       setData(response);
     } catch (error) {
-      setErrors(error);
+      setError(error);
     } finally {
       setLoading(false);
     }
   };
 
-  return { data, loading, errors, fn };
+  return { data, loading, error, fn };
 };
 
 export default useFetch;
