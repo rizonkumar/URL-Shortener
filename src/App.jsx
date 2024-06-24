a5ddb974d6aad789d8891ba7f3874aa5c5bcac25
rizon.rahi <rizon.rahi@ugamsolutions.com>
2024-06-24 14:34:10 +0530

    Feat: Protected Routes for Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Auth from "./pages/Auth";
 import Link from "./pages/Link";
 import RedirectLink from "./pages/RedirectLink";
 import URLProvider from "./Context";
+import RequireAuth from "./components/Require-Auth";
 
 const router = createBrowserRouter([
   {
@@ -18,7 +19,11 @@ const router = createBrowserRouter([
       },
       {
         path: "/dashboard",
-        element: <Dashboard />,
+        element: (
+          <RequireAuth>
+            <Dashboard />
+          </RequireAuth>
+        ),
       },
       {
         path: "/auth",
@@ -26,7 +31,11 @@ const router = createBrowserRouter([
       },
       {
         path: "/link/:id",
-        element: <Link />,
+        element: (
+          <RequireAuth>
+            <Link />
+          </RequireAuth>
+        ),
       },
       {
         path: "/:id",
---
+++ src/components/Require-Auth.jsx
@@ -0,0 +1,18 @@
+import { URLState } from "@/Context";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { BarLoader } from "react-spinners";
+
+const RequireAuth = ({ children }) => {
+  const navigate = useNavigate();
+  const { loading, isAuthenticated } = URLState();
+
+  useEffect(() => {
+    if (!isAuthenticated && loading === false) navigate("/auth");
+  }, [isAuthenticated, loading]);
+
+  if (loading) return <BarLoader width={"100%"} color="#36d7b7" />;
+  if (isAuthenticated) return children;
+};
+
+export default RequireAuth;
